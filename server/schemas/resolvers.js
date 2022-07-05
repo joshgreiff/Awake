f8085dc93cce5c02a1cb81c17dcd6caa837fcb78
Eric Spencer <spencer22eric@gmail.com>
2022-07-05 19:30:30 -0400

    add post field to user model, added post queries to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError, UserInputError } = require('apollo-server-express');
-const { User } = require('../models')
+const { User, Post } = require('../models');
 
 const resolvers = {
     Query: {
@@ -19,7 +19,16 @@ const resolvers = {
                 .populate('level')
                 .populate('exp')
                 .populate('quests')
-                .populate('friends')         
+                .populate('friends')      
+        },
+        // find all posts
+        posts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Post.find(params).sort({ createdAt: -1 });
+        },
+        // find one post by id
+        post: async(parent, { _id }) => {
+            return Post.findOne({ _id });
         }
     },
     Mutations: {}
