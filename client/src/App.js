3812349b02850ef613886f35c6f501a07f81e8a5
kylieogg <kylieogg1@gmail.com>
2022-07-05 20:07:29 -0400

    moved former index components to react index, updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,15 +12,27 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-function App() {
-  return (
-    <div>
-      <nav>test</nav>
-      <main>
-        <Homepage></Homepage>
-      </main>
-    </div>
-  );
-}
+class App extends React.Component {
+  render (){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p> 
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+ }
 
 export default App;
