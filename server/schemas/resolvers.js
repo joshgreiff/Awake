651ac799a9f965c4118dc9eac581ba6359816211
Jay Ford <jay.ford45@gmail.com>
2022-07-11 21:21:19 -0400

    added me query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,6 +7,20 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
         // find all users
+        me: async ( parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({_id: context.user._id})
+                    .select('-__v -password')
+                    .populate('posts')
+                    .populate('coins')
+                    .populate('level')
+                    .populate('exp')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in.')
+        },
         users: async () => {
             return User.find()
                 .select('-__v -password')
