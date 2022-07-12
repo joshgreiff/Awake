4894deb8ac4a2e38d4c686790ae96994ed0c2b25
Eric Spencer <spencer22eric@gmail.com>
2022-07-11 21:55:21 -0400

    render quest query results

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,21 @@
 import { gql } from "@apollo/client"
 
-export const QUERY_USERS=gql`
+export const QUERY_USERS = gql`
     query user($username:String!){
         user(username:$username){
             _id
             username
             email
+        }
+    }
+`
+export const QUERY_QUESTS = gql`
+    query Quests($username: String!) {
+        quests(username: $username) {
+        _id
+        questTitle
+        questDescription
+        createdAt
         }
     }
 `
