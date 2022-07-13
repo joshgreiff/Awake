33217145c7418439e4e1d9c8483b2a362a0e88cd
Adam Belin <adambelin1992@gmail.com>
2022-07-13 19:43:33 -0400

    added dashboard styling and responsive design

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -14,46 +14,46 @@ const Dashboard = () => {
     console.log(data);
     const loggedIn = Auth.loggedIn();
 
-    if(!loggedIn) {
+    if (!loggedIn) {
         return <Navigate to="/login" />
     }
 
     return (
-        <><div className="flex justify-center">
-            <div className="grid grid-cols-12 col-rows-12 py-2 px-4 text-center" >
-                <div className='quest-section bg-gray-400 text-center border-2 border-black col-span-4 row-span-4 bg-grey-400'>
-                    {loading ? (
-                        <div>Loading...</div>
-                    ): (
-                        <DashQuests
+        <><div className="flex justify-center db-center dashboard">
+            <div className='quest-section bg-gray-400 text-center border-2 border-black bg-grey-400'>
+                {loading ? (
+                    <div>Loading...</div>
+                ) : (
+                    <DashQuests
                         quests={data.me.quests}
                         milestones={data?.me.quests.milestones || []}
                         dailies={data?.me.quests.dailies || []}
                         title='Your quests'
-                        />
-                    )}
-                </div>
-                <div className='profile-section border-2 bg-gray-400 text-center border-black col-span-2 bg-gray-400'>
+                    />
+                )}
+            </div>
+            <div className="dashboard-rows">
+                <div className='profile-section border-2 bg-gray-400 text-center border-black bg-gray-400'>
                     {loading ? (
                         <div>Loading...</div>
-                    ):(
+                    ) : (
                         <DashUser
-                        me={data?.me || []}
+                            me={data?.me || []}
                         />
                     )}
                 </div>
-                <div className='post-section border-2 bg-gray-400 text-center border-black col-span-2 bg-gray-400'>
+                <div className='post-section border-2 bg-gray-400 text-center border-black bg-gray-400'>
                     {loading ? (
                         <div>Loading...</div>
-                    ): (
+                    ) : (
                         <DashPosts
-                        posts={data.me.posts}
-                        title='Your posts'
+                            posts={data.me.posts}
+                            title='Your posts'
                         />
                     )}
                 </div>
             </div>
-            </div>
+        </div>
         </>
     )
 }
