aff9a3a613d6f550f48199908c2fc9777d5c52c4
Adam Belin <adambelin1992@gmail.com>
2022-07-12 21:09:00 -0400

    added screen sizing differences

--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -1,30 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import hero from "../../assets/hero/sunset.png";
+import hero from '../../assets/Hero/sunset.png'
 
 export default function Home() {
   return (
     <div>
-        <div className="App">
-      </div>
         <body>               
-            <div>
-                <div className="relative">
-                    <div className="test h-screen w-full bg-no-repeat relative"
-                            style={{
-                            backgroundSize: "auto",
-                            backgroundAttachment: "fixed",
-                            backgroundImage: `url(${hero})`,
-                            }}>
-                        <h1 className="relative text-6xl text-white xl:bg-transparent py-2.5">Change the way you think about your goals</h1>
-                        <h2 className="relative text-xl text-white xl:bg-transparent py-5.5">Click create a quest below to begin your journey</h2><br />
-                        <Link to="/quests">
-                            <button className="relative bg-blue-500 rounded-lg overflow-hidden text-white px-5 py-2.5 group">
-                                <span className="absolute w-0 group-hover:w-full transition-all ease-out duration-300 h-full bg-pink-700 left-0 top-0"></span>
-                                <span className="relative xl:bg-transparent">Create A Quest</span>
-                            </button>
-                        </Link>
-                    </div>
+            <div className="relative">
+                <div className="test h-screen w-full bg-no-repeat relative"
+                        style={{
+                        backgroundSize: "auto",
+                        backgroundAttachment: "fixed",
+                        backgroundImage: `url(${hero})`,
+                        }}>
+                    <h1 className="relative text-6xl text-white xl:bg-transparent py-2.5">Change the way you think about your goals</h1>
+                    <h2 className="relative text-xl text-white xl:bg-transparent py-5.5">Click create a quest below to begin your journey</h2><br />
+                    <Link to="/quests">
+                        <button className="relative bg-blue-500 rounded-lg overflow-hidden text-white px-5 py-2.5 group">
+                            <span className="absolute w-0 group-hover:w-full transition-all ease-out duration-300 h-full bg-pink-600 left-0 top-0"></span>
+                            <span className="relative xl:bg-transparent">Create A Quest</span>
+                        </button>
+                    </Link>
                 </div>
             </div>
         </body>
--- client/src/components/Quests/index.js
+++ client/src/components/Quests/index.js
@@ -3,16 +3,11 @@ import { useQuery } from "@apollo/client"
 import { QUERY_USERS } from "../../utils/queries"
 
 export default function Quests() {
-    const { username:userParam } = useParams()
-    const { loading, data } = useQuery(QUERY_USERS, {
-        variables:{username:userParam}
-    })
-    const user=data?.user || {}
-
-    if(loading) {
-        return <div>loading...</div>
-    }
     return (
-        <div className="text-white">{user.username}</div>
+        <div className="fixed py-4 px-2 flex items-center justify-center w-screen h-screen">
+            <div className="relative box-border h-32 p-4 border-4 ">
+                <h1 className="relative text-white text-6xl ">Quest Data Here</h1>
+            </div>
+        </div>
     )
 }
