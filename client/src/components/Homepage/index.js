3430593e6ae10b3823416aef151f30aef0a63ea4
Adam Belin <adambelin1992@gmail.com>
2022-07-13 14:43:25 -0400

    made homepage fully responsive

--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -3,27 +3,29 @@ import { Link } from 'react-router-dom'
 import hero from "../../assets/Hero/sunset.png";
 
 export default function Home() {
-  return (
-    <div>
-        <body>               
-            <div className="relative">
-                <div className="test h-screen w-full  relative"
+    return (
+        <div>
+            <body>
+                <div className="relative">
+                    <div className="w-screen relative test"
                         style={{
-                        backgroundSize: "auto",
-                        backgroundAttachment: "fixed",
-                        backgroundImage: `url(${hero})`,
+                            backgroundSize: "cover",
+                            backgroundAttachment: "fixed",
+                            backgroundImage: `url(${hero})`,
                         }}>
-                    <h1 className="relative text-6xl text-white xl:bg-transparent py-2.5">Change the way you think about your goals</h1>
-                    <h2 className="relative text-xl text-white xl:bg-transparent py-5.5">Click create a quest below to begin your journey</h2><br />
-                    <Link to="/quests">
-                        <button className="relative bg-blue-500 rounded-lg overflow-hidden text-white px-5 py-2.5 group">
-                            <span className="absolute w-0 group-hover:w-full transition-all ease-out duration-300 h-full bg-pink-600 left-0 top-0"></span>
-                            <span className="relative xl:bg-transparent">Create A Quest</span>
-                        </button>
-                    </Link>
+                        <div className="hp-text">
+                            <h1 className="relative text-4xl md:text-6xl text-white bg-transparent py-2">Change the way you think about your goals</h1>
+                            <h2 className="relative text-2xl md:text-xl text-white bg-transparent py-1.5">Click create a quest below to begin your journey</h2><br />
+                            <Link to="/quests">
+                                <button className="relative bg-blue-500 rounded-lg overflow-hidden text-white px-5 py-2.5 group">
+                                    <span className="absolute w-0 group-hover:w-full transition-all ease-out duration-300 h-full bg-pink-600 left-0 top-0"></span>
+                                    <span className="relative bg-transparent">Create A Quest</span>
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </body>
-    </div>
-  )
+            </body>
+        </div>
+    )
 }
