c7dbc75df32a6a09993281c520e4ff30da566f06
kylieogg <kylieogg1@gmail.com>
2022-07-14 12:43:37 -0400

    fixed styling class to center shop image

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -5,17 +5,16 @@ import sign from "../../assets/shop/shop-sign.png"
 export default function Shop() {
     return (   
         <div>
-            <body className="bg-cover bg-center bg-gradient-to-b from-blue-400">
+            <body className="bg-gradient-to-b from-blue-400">
                 <div>
-                    <div className="relative py-10">
-                        <div className="test h-screen w-full bg-no-repeat relative"
+                    <div className="flex items-center">
+                        <div className="shopscreen h-screen w-full bg-no-repeat relative"
                             style={{
                             backgroundSize: "auto",
                             backgroundAttachment: "relative",
                             backgroundImage: `url(${shop})`,
                             }}>
                         </div>
-                    
                     </div>
                 </div>
             </body>
