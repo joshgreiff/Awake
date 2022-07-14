8259326e9671cf1bef97492ea4b5e7fb7c09c46c
Jay Ford <jay.ford45@gmail.com>
2022-07-14 14:30:12 -0400

    added fonts

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -24,34 +24,34 @@ export default function Navbar() {
             <img className="Awake" src={logo} alt="React logo"></img> 
         </Link>
         <ul>
-            <CustomLink to="/Dashboard">Dashboard</CustomLink>
-            <CustomLink to="/Social">Social</CustomLink>
-            <CustomLink to="/Communities">Communities</CustomLink>
-            <CustomLink to="/Shop">Shop</CustomLink>
+            <CustomLink to="/Dashboard" className="font">Dashboard</CustomLink>
+            <CustomLink to="/Social" className="font">Social</CustomLink>
+            <CustomLink to="/Communities" className="font">Communities</CustomLink>
+            <CustomLink to="/Shop" className="font">Shop</CustomLink>
         </ul>
         <ul>
             
             {Auth.loggedIn() ? (
             <>
               {/* <Link to ="/profile">Me</Link> */}
-              <a href="/" onClick={logout}>
+              <a href="/" onClick={logout} className="font">
                 Logout
               </a>
               <div className='flex align-middle'>
                 <img src={coin} width={30} className='bg-nav-grey'/>
-                <div className='pt-3 pl-1 text-black bg-nav-grey'>{data.me.coins}</div>
+                <div className='pt-3 pl-1 text-black bg-nav-grey font'>{data.me.coins}</div>
               </div>
               <div className='flex align-middle'>
-                <div className='pt-3 pl-1 text-black bg-nav-grey'>Level:</div>
+                <div className='pt-3 pl-1 text-black bg-nav-grey font'>Level:</div>
                 
-                <div className='pt-3 pl-1 text-black bg-nav-grey'>{data.me.level}</div>
+                <div className='pt-3 pl-1 text-black bg-nav-grey font'>{data.me.level}</div>
               </div> 
             
             </>
           ) : (
             <>
-                <Link to="/Login">Login</Link>
-                <Link to="/Signup">Sign Up!</Link>
+                <Link to="/Login font">Login</Link>
+                <Link to="/Signup font">Sign Up!</Link>
             </>
           )}
         </ul>
