108c7a440c6e4a22f08631976e2450c7f8c7a8e4
kylieogg <kylieogg1@gmail.com>
2022-07-13 20:09:33 -0400

    updated css to fill background

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import shop from "../../assets/shop/shop.png"
+import sign from "../../assets/shop/shop-sign.png"
 
 export default function Shop() {
     return (   
         <div>
-
-            <body className=" bg-cover bg-gradient-to-b from-blue-400">
+            <div className="App"></div>
+            <body className="bg-cover bg-center bg-gradient-to-b from-blue-400">
                 <div>
-                    <div className="relative flex justify-center py-10">
-                        <img className=""></img><img src={shop} alt=""></img>
+                    <div className="relative py-10">
+                        <div className="test h-screen w-full bg-no-repeat relative"
+                            style={{
+                            backgroundSize: "auto",
+                            backgroundAttachment: "relative",
+                            backgroundImage: `url(${shop})`,
+                            }}>
+                        </div>
+                    
                     </div>
                 </div>
             </body>
