a58fd4abcf425173a871f14a27f8b056b79c01da
jgreiff-ds <jgreiff@dancorsolutions.com>
2023-03-16 01:01:33 -0400

    mjcxm

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import './output.css';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context'
 
