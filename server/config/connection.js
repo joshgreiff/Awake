26925071f7a83c0a2b125feadd71be04b304780a
jgreiff-ds <josh@greiff.org>
2024-12-11 11:18:55 -0500

    fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://joshgreiff:KipHflNwlIwi6DPN@awake.owiki4e.mongodb.net/test',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
